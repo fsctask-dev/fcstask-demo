34d11590a3793dd6a250c2c6145517b533b9bb0a
Yulia Baranova <byuliashkaa@gmail.com>
2026-02-16 18:39:19 +0300

    Fix metrics-consumer

--- internal/cmd/metrics-consumer/main.go
+++ internal/cmd/metrics-consumer/main.go
@@ -3,6 +3,7 @@ package metrics_consumer
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"log"
 	"os"
 	"os/signal"
@@ -36,43 +37,49 @@ func main() {
 
 	log.Printf("[Metrics Consumer] Started. Topic: %s", cfg.Kafka.TopicMetrics)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
 	ctx := context.Background()
+	go func() {
+		<-sigchan
+		log.Println("[Metrics Consumer] Shutting down...")
+		cancel()
+	}()
 
-	for {
-		select {
-		case <-sigchan:
-			log.Println("[Metrics Consumer] Shutting down...")
-			return
-		default:
-			msg, err := reader.ReadMessage(ctx)
-			if err != nil {
-				log.Println("[Metrics Consumer] Error reading message:", err)
-				continue
+	for ctx.Err() == nil {
+		msg, err := reader.ReadMessage(ctx)
+		if err != nil {
+			if errors.Is(err, context.Canceled) {
+				break
 			}
+			log.Println("[Metrics Consumer] Error reading message:", err)
+			continue
+		}
 
-			var metric kafkaMetric.Metric
-			err = json.Unmarshal(msg.Value, &metric)
-			if err != nil {
-				log.Println("[Metrics Consumer] Error unmarshalling message:", err)
-				continue
-			}
+		var metric kafkaMetric.Metric
+		err = json.Unmarshal(msg.Value, &metric)
+		if err != nil {
+			log.Println("[Metrics Consumer] Error unmarshalling message:", err)
+			continue
+		}
 
-			//пока какой-то лог, потом это всё в бд будет кидаться
-			tags := ""
-			if len(metric.Tags) > 0 {
-				tagsBytes, _ := json.Marshal(metric.Tags)
-				tags = string(tagsBytes)
-			}
+		//пока какой-то лог, потом это всё в бд будет кидаться
+		tags := ""
+		if len(metric.Tags) > 0 {
+			tagsBytes, _ := json.Marshal(metric.Tags)
+			tags = string(tagsBytes)
+		}
 
-			log.Printf("[METRIC] time=%s name=%s value=%.2f tags=%s",
-				metric.Timestamp.Format(time.RFC3339),
-				metric.Name,
-				metric.Value,
-				tags,
-			)
+		log.Printf("[METRIC] time=%s name=%s value=%.2f tags=%s",
+			metric.Timestamp.Format(time.RFC3339),
+			metric.Name,
+			metric.Value,
+			tags,
+		)
 		}
 	}
 
